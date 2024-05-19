fe5deb74bbad6f53f0163b2cfbfea295cc27f4d6
Kiss Krisztian <kkriszel@gmail.com>
2024-05-19 18:33:38 +0300

    Update with fixed homepage

---
+++ precache-manifest.a50d477a44261340258c10bf42e58bf5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a3fcb139d59a15fbda1e55642da4f52",
+    "url": "/json-flashcards-deployment/index.html"
+  },
+  {
+    "revision": "540f58b3ec28e20cffdb",
+    "url": "/json-flashcards-deployment/static/css/main.3801137c.chunk.css"
+  },
+  {
+    "revision": "c04f3dbed37cf68d1980",
+    "url": "/json-flashcards-deployment/static/js/2.5454327f.chunk.js"
+  },
+  {
+    "revision": "540f58b3ec28e20cffdb",
+    "url": "/json-flashcards-deployment/static/js/main.53a05232.chunk.js"
+  },
+  {
+    "revision": "63b3a3c899fddb3b14ac",
+    "url": "/json-flashcards-deployment/static/js/runtime-main.5000e184.js"
+  },
+  {
+    "revision": "bb8217dddd0791ac897bea4ddde404b4",
+    "url": "/json-flashcards-deployment/static/media/correct.bb8217dd.svg"
+  },
+  {
+    "revision": "798001c02d7fb048d391946ba9f14733",
+    "url": "/json-flashcards-deployment/static/media/drag.798001c0.svg"
+  },
+  {
+    "revision": "f3cf93b3844a68d3eeb0ba3d7daac326",
+    "url": "/json-flashcards-deployment/static/media/gear.f3cf93b3.svg"
+  },
+  {
+    "revision": "0a607ec2b1805d6ab361e309ca53a5c9",
+    "url": "/json-flashcards-deployment/static/media/help.0a607ec2.svg"
+  },
+  {
+    "revision": "26fb2114a9731e2b905925fe4450dcc6",
+    "url": "/json-flashcards-deployment/static/media/incorrect.26fb2114.svg"
+  },
+  {
+    "revision": "5dacd004927da04b74e271b176c3dee0",
+    "url": "/json-flashcards-deployment/static/media/quiz-bg.5dacd004.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4069fe6097f45a08f98dd2e66bce615d.js"
+  "/json-flashcards-deployment/precache-manifest.a50d477a44261340258c10bf42e58bf5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/json-flashcards-deployment/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
