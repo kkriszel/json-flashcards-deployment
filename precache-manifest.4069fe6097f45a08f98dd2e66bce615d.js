398456fb65b18934d46792512934626e4f4f9dd2
Kiss Krisztian <kkriszel@gmail.com>
2024-05-19 18:13:20 +0300

    Add build stuff

---
+++ precache-manifest.4069fe6097f45a08f98dd2e66bce615d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a32f387eb1f33ebe42bf8bcd6913a961",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d8625c7d749cfacd620",
+    "url": "/static/css/main.e9aa5616.chunk.css"
+  },
+  {
+    "revision": "c04f3dbed37cf68d1980",
+    "url": "/static/js/2.5454327f.chunk.js"
+  },
+  {
+    "revision": "8d8625c7d749cfacd620",
+    "url": "/static/js/main.9577ec20.chunk.js"
+  },
+  {
+    "revision": "bf33c2bfd38ae7673d65",
+    "url": "/static/js/runtime-main.19f1cca5.js"
+  },
+  {
+    "revision": "bb8217dddd0791ac897bea4ddde404b4",
+    "url": "/static/media/correct.bb8217dd.svg"
+  },
+  {
+    "revision": "798001c02d7fb048d391946ba9f14733",
+    "url": "/static/media/drag.798001c0.svg"
+  },
+  {
+    "revision": "f3cf93b3844a68d3eeb0ba3d7daac326",
+    "url": "/static/media/gear.f3cf93b3.svg"
+  },
+  {
+    "revision": "0a607ec2b1805d6ab361e309ca53a5c9",
+    "url": "/static/media/help.0a607ec2.svg"
+  },
+  {
+    "revision": "26fb2114a9731e2b905925fe4450dcc6",
+    "url": "/static/media/incorrect.26fb2114.svg"
+  },
+  {
+    "revision": "5dacd004927da04b74e271b176c3dee0",
+    "url": "/static/media/quiz-bg.5dacd004.png"
+  }
+]);
